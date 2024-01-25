b791d83bb9cb503d55ff4fd4b6cf8dff7d131cf5
zhangzhuang08 <zhangzhuang08@meituan.com>
2024-01-25 20:13:14 +0800

    增加一个refer，帮助理解类型里的 Equal type 实现

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -4,6 +4,7 @@ export type ExpectFalse<T extends false> = T
 export type IsTrue<T extends true> = T
 export type IsFalse<T extends false> = T
 
+// https://github.com/microsoft/TypeScript/issues/27024
 export type Equal<X, Y> =
   (<T>() => T extends X ? 1 : 2) extends
   (<T>() => T extends Y ? 1 : 2) ? true : false
